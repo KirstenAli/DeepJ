494f97e501b7622dc325d3989edad1bed50234d0
Kirsten Ali <ikirstenhd@gmail.com>
2023-06-25 17:16:45 +0100

    initial commit.

--- src/main/java/org/jbackprop/Neuron.java
+++ src/main/java/org/jbackprop/Neuron.java
@@ -26,9 +26,9 @@ public class Neuron {
     }
 
     public double calculateNet(){
-        for(Connection connection: inputConnections){
+        for(Connection connection: inputConnections)
             net+= connection.calculateProduct();
-        }
+
         return net+=bias;
     }
 
