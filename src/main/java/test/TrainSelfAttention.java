a4602e7fb5db222da5b2da3800cef1eac2d62679
kirsten <ikirstenhd@gmail.com>
2025-05-18 00:23:48 +0100

    1.code refactor
    2.test for integrating SelfAttentionLayer with NN
    
    3. docs updated

--- src/main/java/test/TrainSelfAttention.java
+++ src/main/java/test/TrainSelfAttention.java
@@ -6,22 +6,22 @@ import org.DeepJ.transformer.Tensor;
 public class TrainSelfAttention {
     public static void main(String[] args) {
         int dim = 4;
-        Tensor input = new Tensor(new double[][] {
+        var input = new Tensor(new double[][] {
                 {1, 0, 0, 0},
                 {0, 1, 0, 0},
                 {0, 0, 1, 0}
         });
-        Tensor target = new Tensor(new double[][] {
+        var target = new Tensor(new double[][] {
                 {1, 0, 0, 0},
                 {0, 1, 0, 0},
                 {0, 0, 1, 0}
         });
 
-        SelfAttentionLayer attn = new SelfAttentionLayer(dim);
+        var attn = new SelfAttentionLayer(dim);
 
         for (int epoch = 0; epoch < 100000; epoch++) {
-            Tensor output = attn.forward(input);
-            Tensor dLoss = output.subtract(target);
+            var output = attn.forward(input);
+            var dLoss = output.subtract(target);
             if (epoch % 100 == 0) {
                 System.out.printf("Epoch %d - Loss: %.6f\n", epoch, output.mseLoss(target));
             }
